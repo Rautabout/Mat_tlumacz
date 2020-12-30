1573e03108fef5608916dae44093134339e51e3a
Karolina Wieczorek <karolinachrop@gmail.com>
2020-12-30 16:28:35 +0100

    Changes in dictionary

--- Model/ConvertingFunctions/BaseForTrees.cs
+++ Model/ConvertingFunctions/BaseForTrees.cs
@@ -2,9 +2,34 @@
 
 public class BaseForTrees
 {
-		var symbols = new Dictionary<String, String[3]>()
+		var symbols = new Dictionary<String, String[3]>() //Kolejność wartości Doc, Tex, MathML
 		{
-
+			{ "+", ["+","+",""]},
+			{ "-", ["-","-",""]},
+			{ "*", ["*","\\cdot",""]},
+			{ "^", ["^","^",""]},
+			{ "_", ["_","_",""]},
+			{ "/", ["/","\\frac",""]},
+			{ "sqrt", ["\\sqrt","\\sqrt",""]},
+			{ "int", ["\\int","\\int",""]},
+			{ "alpha", ["\\alpha","\\alpha",""]},
+			{ "beta", ["\\beta","\\beta",""]},
+			{ "gamma", ["\\gamma","\\gamma",""]},
+			{ "delta", ["\\delta","\\delta",""]},
+			{ "sin", ["sin","\\sin",""]},
+			{ "cos", ["cos","\\cos",""]},
+			{ "tan", ["tan","\\tan",""]},
+			{ "cot", ["cot","\\cot",""]},
+			{ "<=", ["\\le","\\leq",""]},
+			{ ">=", ["\\ge","\\geq",""]},
+			{ "<", ["<","<",""]},
+			{ ">", [">",">",""]},
+			{ "in", ["\\in","\\in",""]},
+			{ "notin", ["\\notelement","\\notin",""]},
+			{ "and", ["\\vedge","\\land",""]},
+			{ "or", ["\\vee","\\lor",""]},
+			{ "pi", ["\\pi","\\pi",""]},
+			{ "e", ["exp(1)","e",""]}
 		};
 
 	class Node
