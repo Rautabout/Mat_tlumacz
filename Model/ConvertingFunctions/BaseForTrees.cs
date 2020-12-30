17ffdde82327b10feb9452682ac1faf8bd605467
Katarzyna Ptaszynska <ptaszynska.katarzyna95@gmail.com>
2020-12-30 14:06:33 +0100

    new file - base for trees. Incuded trees class and, in future, dictionary for maths symbols.

---
+++ Model/ConvertingFunctions/BaseForTrees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class BaseForTrees
+{
+	public BaseForTrees()
+	{
+		
+		class Node
+		{
+		public int value;
+		public Node left;
+		public Node right;
+
+		public Node(int value)
+		{
+			this.value = velue;
+			this.left = null;
+			this.right = null;
+		}
+
+		class Tree
+		{
+			public Node root;
+			public Tree()
+			{
+				root = null;
+			}
+			bool isEmpty()
+			{
+				return root == null;
+			}
+		}
+	};
+
+}
+}
--- Model/ConvertingFunctions/ToTree.cs
+++ Model/ConvertingFunctions/ToTree.cs
@@ -4,5 +4,9 @@ public class ToTree
 {
 	public ToTree()
 	{
+		public static void texToTree(string input)
+        {
+
+        }
 	}
 }
