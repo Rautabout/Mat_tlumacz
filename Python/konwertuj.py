cab16821d1b2c06869dfff01d8f82620ee910dba
Katarzyna Ptaszynska <ptaszynska.katarzyna95@gmail.com>
2021-01-20 01:32:13 +0100

    View in Python

--- Python/konwertuj.py
+++ Python/konwertuj.py
@@ -2,6 +2,8 @@ import DocToTree
 import TreeToDoc
 import TexToTree
 import TreeToTex
+import MathMlToTree
+import TreeToMathMl
 import sys
 
 
@@ -10,11 +12,12 @@ def convert(inL,outL,string):
 
     languages['tex'] = [TexToTree.texToTree, TreeToTex.treeToTex]
     languages['doc'] = [DocToTree.docToTree, TreeToDoc.treeToDoc]
+    # languages['mathml']=[MathMlToTree.mathMlToTree, ]
             
     return languages[outL][1](languages[inL][0](string))
 
-inL = sys.argv[1]
-outL = sys.argv[2]
-string =sys.argv[3]
-result = convert(inL,outL,string)
-print(result)
+# inL = sys.argv[1]
+# outL = sys.argv[2]
+# string =sys.argv[3]
+# result = convert(inL,outL,string)
+# print(result)
